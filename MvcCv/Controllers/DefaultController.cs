50c478e4ff48a34a9e473aa959a52fad46806914
emineegnss <80625362+emineegnss@users.noreply.github.com>
2023-11-29 23:41:25 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entities;
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities1 db = new DbCvEntities1();
+        public ActionResult Index()
+        {
+            var items = db.TblAbout.ToList();
+            return View(items);
+        }
+        public PartialViewResult Experience()
+        {
+            var experience = db.TblExperience.ToList();
+            return PartialView(experience);
+        }
+        public PartialViewResult Education()
+        {
+            var education = db.TblEducation.ToList();
+            return PartialView(education);
+        }
+        public PartialViewResult Skills()
+        {
+            var skill = db.TblSkills.ToList();
+            return PartialView(skill);
+        }
+        public PartialViewResult Interests()
+        {
+            var interests = db.TblInterests.ToList();
+            return PartialView(interests);
+        }
+    }
+}
